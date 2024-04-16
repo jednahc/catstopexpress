403e2877460eaf02987ca96d4eed377c14502cf8
jedjitsu <159091680+jedjitsu@users.noreply.github.com>
2024-04-16 14:44:10 +0800

    added cse files

---
+++ cse.js
@@ -0,0 +1,11 @@
+
+//* functions
+function rollDice() {
+  let diceNum = [];
+  for(let i = 0; i < 5; i++){
+  diceNum.push(Math.floor(Math.random() * 6) + 1);
+return diceNum;
+}
+}
+console.log(rollDice());
+
